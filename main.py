6d5b9aca3548145f3d3dad8cb0256a8759c53222
islomrio <mers1800e@inbox.ru>
2026-08-24 08:20:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ async def monitor(context: ContextTypes.DEFAULT_TYPE):
                     status="red"
 
                 )
-                try:
+            
                     with open(card, "rb") as photo:
                         await context.bot.send_photo(
                             chat_id=chat_id,
