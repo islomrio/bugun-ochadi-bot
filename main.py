937d7d04b02f909acf29370d9b705d58300a3a29
islomrio <mers1800e@inbox.ru>
2026-08-24 08:07:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -184,14 +184,14 @@ async def monitor(context: ContextTypes.DEFAULT_TYPE):
                     status="red"
 
                 )
-
+           try:
                 with open(card, "rb") as photo:
                     await context.bot.send_photo(
                         chat_id=chat_id,
                         photo=photo
                     )
 
-            except Exception as e:
+       except Exception as e:
                 print(e)
 
 app = Application.builder().token(TOKEN).build()
